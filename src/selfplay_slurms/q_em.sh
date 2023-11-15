5d6d8068d4fae12f2172d00f82de52306a70a6df
Ian Wu <ianwu13@me.com>
2023-11-14 21:17:27 -0800

    making selfplay heatmap

---
+++ src/selfplay_slurms/q_em.sh
@@ -0,0 +1,15 @@
+sbatch casino_fair_fair.slurm          
+sbatch casino_sv_selfish.slurm         
+sbatch dnd_selfish_selfish.slurm
+
+sbatch casino_selfish_fair.slurm       
+sbatch casino_sv_sv.slurm              
+sbatch dnd_sv_fair.slurm
+
+sbatch casino_selfish_selfish.slurm    
+sbatch dnd_fair_fair.slurm             
+sbatch dnd_sv_selfish.slurm
+
+sbatch casino_sv_fair.slurm            
+sbatch dnd_selfish_fair.slurm          
+sbatch dnd_sv_sv.slurm
